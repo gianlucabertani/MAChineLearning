5d636b6404da7ceeecd0a23e5ef971a5f015eb59
Gianluca Bertani <gianluca.bertani@email.it>
2015-04-12 17:21:55 +0200

    Initial Commit

---
+++ MAChineLearning/MAChineLearning.h
@@ -0,0 +1,19 @@
+//
+//  MAChineLearning.h
+//  MAChineLearning
+//
+//  Created by Gianluca Bertani on 12/04/15.
+//  Copyright (c) 2015 Flying Dolphin Studio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MAChineLearning.
+FOUNDATION_EXPORT double MAChineLearningVersionNumber;
+
+//! Project version string for MAChineLearning.
+FOUNDATION_EXPORT const unsigned char MAChineLearningVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MAChineLearning/PublicHeader.h>
+
+
